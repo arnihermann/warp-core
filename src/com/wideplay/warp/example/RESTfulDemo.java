81eeecf18ec5ec00977045bf6780ab5f5a1d3c58
dhanji <dhanji@e7d256a5-fc2b-0410-8b46-a5493d102834>
2007-08-07 08:20:20 +0000

    RESTful URIs support added (with param injection).
    some cleanup and exposed Request & Response via injector.
    
    git-svn-id: http://warp-core.googlecode.com/svn/trunk@116 e7d256a5-fc2b-0410-8b46-a5493d102834

---
+++ src/com/wideplay/warp/example/RESTfulDemo.java
@@ -0,0 +1,25 @@
+package com.wideplay.warp.example;
+
+import com.wideplay.warp.annotations.OnEvent;
+import com.wideplay.warp.annotations.URIMapping;
+import com.wideplay.warp.annotations.event.PreRender;
+
+/**
+ * Created with IntelliJ IDEA.
+ * On: 20/03/2007
+ *
+ * @author Dhanji R. Prasanna
+ * @since 1.0
+ */
+@URIMapping("/movie/{name}")
+public class RESTfulDemo {
+    private String movie;
+
+    @OnEvent @PreRender public void onLoadPage(String name) {
+        this.movie = name;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+}
